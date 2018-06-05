535218edff6be93b42e6d0913ef84a214408b5e3
Ali <alikiyand@gmail.com>
2018-06-05 14:48:16 +0100

    adding read api endpoint to the restful API

--- models/Post.php
+++ models/Post.php
@@ -84,6 +84,41 @@ class Post{
 
     }
 
+    //Create Post
+    public function create(){
+      // Create Query
+      // Using name parameters
+      $query  = 'INSERT INTO '. $this->table . '
+      SET
+         title = :title,
+         body = :body,
+         author = :author,
+         category_id = :category_id';
+      // Prepate statement
+      $stmt = $this->conn->prepare($query);
+      //Clean Data (security measures)
+      $this->title  = htmlspecialchars(strip_tags($this->title));
+      $this->body  = htmlspecialchars(strip_tags($this->body));
+      $this->author  = htmlspecialchars(strip_tags($this->author));
+      $this->category_id  = htmlspecialchars(strip_tags($this->category_id));
+
+      // Binding data
+      $stmt->bindParam(':title', $this->title);
+      $stmt->bindParam(':body', $this->body);
+      $stmt->bindParam(':author', $this->author);
+      $stmt->bindParam(':category_id', $this->category_id);
+
+      // Execute Query
+      if($stmt->execute()){
+        return true;
+      }
+      // print error if something goes wrong
+      printf("Error: %s.\n", $stmt->error);
+      return false;
+    }
+
+
+
 
 }
 
